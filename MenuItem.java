e996e9e3f18fa24a45a2626c2e146cff5f4d51be
balnurkuantay <balnurkuantay@gmail.com>
2026-01-03 23:56:05 +0500

    Create MenuItem class for menu management

---
+++ MenuItem.java
@@ -0,0 +1,54 @@
+import java.util.Objects;
+
+public class MenuItem extends BaseEntity {
+    private String name;
+    private double price;
+    private String category;
+
+    public MenuItem(int id, String name, double price, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    @Override
+    public String getInfo() {
+        return name + " - " + price;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuItem{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MenuItem)) return false;
+        MenuItem menuItem = (MenuItem) o;
+        return id == menuItem.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
